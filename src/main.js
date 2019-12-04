68c1ed08379d3858b72be0577c08adc6852851ca
George Acosta <georgeacostar@gmail.com>
2019-12-03 23:16:53 -0800

    add async func

--- src/main.js
+++ src/main.js
@@ -7,39 +7,5 @@ import './styles.css';
 
 $(document).ready(function() {
 
-  // THIS FUNCTION WILL SEARCH ARTIST BY NAME
-  $('#artistSection').click(function(event) {
-    event.preventDefault();
-
-    const inputedArtist = $('#inputedName').val();
-    // THIS WILL RUN API CALL FOR ARTIST BY NAME
-    (async () => {
-      let newArtistByName = new ArtistByName();
-      const response = await newArtistByName.getArtistByName(inputedArtist);
-      console.log(response);
-
-      getElements(response);
-    })();
-
-    // THIS WILL DISPLAY API FUNCTION RETURN ON (DOM)
-    function getElements(response) {
-
-      // THIS WILL CATCH IF SEARCH RESULTS ARE BAD(NON ERROR TYPE)
-      const doesArtistExist = response.meta.total > 0;
-
-      const canArtistBeFound = (inputedArtist) => {
-        if (doesArtistExist){
-          return `Please see details below for: ${inputedArtist} `;
-        } else {
-          return `No artist in our database match ${inputedArtist}, please try another search term`;
-        }
-      }
-
-      $("#showArtistByNameSection").text(canArtistBeFound(inputedArtist));
-
-      // ARTIST 1 DETAILS
-      // TARGETED INFO BELOW
-
-    }
-  });
+  
 });
